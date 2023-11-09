1b644229d9b16368cb78ab9057f768cf3de2f10a
90536 <90536@DESKTOP-FCHRJ42>
2023-11-09 16:27:44 +0300

    Add project files.

---
+++ SignalR.BusinessLayer/Concrete/ProductManager.cs
@@ -0,0 +1,45 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private readonly IProductService _productService;
+
+        public ProductManager(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public void TAdd(Product entity)
+        {
+            _productService.TAdd(entity);
+        }
+
+        public void TDelete(Product entity)
+        {
+            _productService.TDelete(entity);
+        }
+
+        public Product TGetByID(int id)
+        {
+            return _productService.TGetByID(id);    
+        }
+
+        public List<Product> TGetListAll()
+        {
+            return _productService.TGetListAll();
+        }
+
+        public void TUpdate(Product entity)
+        {
+            _productService.TUpdate(entity);
+        }
+    }
+}
---
+++ SinalRWebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SinalRWebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
